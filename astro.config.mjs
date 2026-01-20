36de36b7e5ed56ffd6fee2bf4324e0c03e2a4ec2
jgmachine <jgmachine@gmail.com>
2026-01-19 20:08:54 -0800

    feat: Add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jessegeron.github.io',
+  base: '/jessegeron.com',
   vite: {
     plugins: [tailwindcss()]
   }
